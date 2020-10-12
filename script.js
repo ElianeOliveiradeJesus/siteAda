22d60c031db5d0e9473828f6755dda4d01b3a7d2
ElianeOliveiradeJesus <41996292+ElianeOliveiradeJesus@users.noreply.github.com>
2020-10-12 16:12:11 -0300

    Create script.js
    Script pronto

---
+++ script.js
@@ -0,0 +1,22 @@
+/*if (document.getElementByID("nome").value != "" &&document.getElementByID("email").value != ""){document.getElementByID("botaoEnviar").addEventListener("click", validaFormulario )
+
+function validaFormulario(){
+
+  if(document.getElementById("nome").value != "" &&
+     document.getElementById("email").value != "" &&
+     document.getElementById("telefone").value != "" ){
+
+    alert("Prontinho! você receberá as novidades por email.")
+  }else{
+    alert("Por favor, preencha os campos nome e email.")
+  }
+}
+    
+    
+/*if (document.getElementByID("nome").value != "" &&document.getElementByID("email").value != ""){
+    alert("Prontinho! Você receberá as novidades por email.")
+      }else{
+        alert("Por favor, preencha os campos nome e email!")
+      }*/
+    
+    /*Ao clicar no botão enviar os scripts acima irão acontecer
